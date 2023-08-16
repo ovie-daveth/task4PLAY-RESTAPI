ec5e22032eee7fd539641d75c570eb20345f759c
ovie-davet <oviedavid77@mail.com>
2023-08-16 02:46:15 +0100

    finished ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,63 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {BiSolidEditAlt} from "react-icons/bi"
+import {MdDelete} from "react-icons/md"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [title, setTitle] = useState('')
+  const [desc, setDesc] = useState('')
+
+  const addTask  = async (e) => {
+    e.preventDefault()
+    console.log(title,desc)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="main">
+      <h1>A Simple Test-TaskApp to test my backend API</h1>
+      <p>Hey, there!, dont  mind my frontend, this is not the focus!</p>
+      <form>
+        <h2>Add a task!</h2>
+        <div className="form-field">
+            <input type="text" placeholder='Title...' value={title} onChange={(e) =>setTitle(e.target.value)} />
+            <textarea type="text" placeholder='content...' rows="5" value={desc} onChange={(e) =>setDesc(e.target.value)} />
+            <button onClick={addTask}>Add Task</button>
+        </div>
+      </form>
+      <div className="content">
+        <div className="content-cont">
+          <div className="cont">
+          <input type="checkbox" className="checkbox" />
+          <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="func">
+          <BiSolidEditAlt className="edit" />
+          <MdDelete className="delete" />
+        </div>
+        </div>
+        <div className="content-cont">
+          <div className="cont">
+          <input type="checkbox" className="checkbox" />
+          <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="func">
+          <BiSolidEditAlt  className="edit" />
+          <MdDelete className="delete" />
+        </div>
+        </div>
+        <div className="content-cont">
+          <div className="cont">
+          <input type="checkbox" className="checkbox" />
+          <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="func">
+          <BiSolidEditAlt className="edit"  />
+          <MdDelete className="delete" />
+        </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
